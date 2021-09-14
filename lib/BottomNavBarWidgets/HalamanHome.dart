4c33bf08e9605ce6b6bcc2eda6ec662c1cc5e936
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2021-09-14 13:33:54 +0700

    HomePage & Search Bar Update

--- lib/BottomNavBarWidgets/HalamanHome.dart
+++ lib/BottomNavBarWidgets/HalamanHome.dart
@@ -14,18 +14,21 @@ class HalamanHome extends StatefulWidget {
 class _HalamanHomeState extends State<HalamanHome> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset('assets/images/HomePage.jpg', fit: BoxFit.contain,),
-          ClipRect(
-            child: Container(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              alignment: Alignment(0, -0.995),
+              child: Image.asset('assets/images/HomePage.jpg',scale: 0.9)
+            ),
+            Container(
+              alignment: Alignment.topCenter,
               child: SearchBar(),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
