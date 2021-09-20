ca8af6f30a238de6e00b62e4fcc9a3e5621d98a6
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2021-09-20 21:09:18 +0700

    HomeButtonGroup (HomePage)

--- lib/BottomNavBarWidgets/HomePage/HomeButtonGroup.dart
+++ lib/BottomNavBarWidgets/HomePage/HomeButtonGroup.dart
@@ -12,23 +12,61 @@ class HomeButtonGroup extends StatelessWidget {
       ),
       child: IntrinsicHeight(
         child: new Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text('Foo'),
+            Container(
+              child: GestureDetector(
+                child: Image.asset('assets/icons/Scan.png',
+                  width: 24.0, height: 24.0, color: Colors.black87,
+                ),
+                onTap: () {
+                  print('Scan Button Pressed');
+                },
+              ),
+              padding: EdgeInsets.only(left: 20.0, right: 5.0),
+            ),
             VerticalDivider(
               color: Color.fromRGBO(197, 197, 197, 100),
-              thickness: 0.5,
+              thickness: 0.7,
               indent: 8.0,
               endIndent: 8.0,
             ),
-            Text('Ble'),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    child: Image.asset('assets/icons/wallet.png'),
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                  ),
+                  Padding(
+                    child: Text('Rp.200.000', style: TextStyle(fontSize: 12,
+                        fontFamily: 'Raleway'),),
+                    padding: EdgeInsets.only(right: 25.0),
+                  ),
+                ],
+              ),
+            ),
             VerticalDivider(
               color: Color.fromRGBO(197, 197, 197, 100),
-              thickness: 0.5,
+              thickness: 0.7,
               indent: 8.0,
               endIndent: 8.0,
             ),
-            Text('Boo'),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    child: Image.asset('assets/icons/point.png'),
+                    padding: EdgeInsets.only(left: 5.0,right: 10.0),
+                  ),
+                  Padding(
+                    child: Text('172 koin', style: TextStyle(fontSize: 12,
+                        fontFamily: 'Raleway'),),
+                    padding: EdgeInsets.zero,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
