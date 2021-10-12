6f41d41ede45cf3fef1ed2ac31b92c59b72ae026
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2021-10-12 11:05:13 +0700

    VisitPage Update (Form)

--- lib/ActivityWidgets/HalamanVisit.dart
+++ lib/ActivityWidgets/HalamanVisit.dart
@@ -1,24 +1,98 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    title: 'Visit',
-    home: new HalamanVisit(),
-  ));
+class HalamanVisit extends StatefulWidget {
+  const HalamanVisit({Key? key}) : super(key: key);
+
+  @override
+  _HalamanVisitState createState() => _HalamanVisitState();
 }
 
-class HalamanVisit extends StatelessWidget {
+class _HalamanVisitState extends State<HalamanVisit> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: new Text('Visit Langsung'),
-          backgroundColor: Color.fromRGBO(95, 37, 224, 100),
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Padding(
+              padding: EdgeInsets.only(left: 30.0,right: 0.0),
+              child: Image.asset('assets/icons/LogoVisitSekarang.png'),
+            ),
+            title: new Text('Visit Sekarang'),
+            backgroundColor: Color.fromRGBO(95, 37, 224, 100),
+          ),
+
+          body: Form(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Masukan Nama Lengkap Pasien',
+                          labelText: 'Nama Pasien',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Masukan Usia Pasien',
+                          labelText: 'Usia Pasien',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Masukan Jenis Kelamin Pasien',
+                          labelText: 'Jenis Kelamin',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Masukan Keluhan Pasien',
+                          labelText: 'Keluhan Utama',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Masukan tanggal awal sakit',
+                          labelText: 'Keluhan Sejak',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Masukan Jenis Kelamin Pasien',
+                          labelText: 'Diagnosa Dokter (Jika Ada)',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Kondisi Pasien Sekarang',
+                          labelText: 'Kondisi Pasien Sekarang',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Permintaan Khusus',
+                          labelText: 'Permintaan Khusus',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextFormField(
+                      decoration: new InputDecoration(
+                          hintText: 'Dropdown',
+                          labelText: 'Pilih Tanggal Terapi',
+                          icon: Icon(Icons.people)),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child : Text('Lanjutkan Pemesanan'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
-        body: new Container(
-            padding: EdgeInsets.all(20.0),
-            child: new Center(
-              child: new Text('Ini halaman Visit.'),
-            )));
+      );
   }
 }
