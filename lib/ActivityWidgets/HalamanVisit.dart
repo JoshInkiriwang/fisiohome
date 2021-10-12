cb1a1382e1b94e656a9ce49e717abf50cd48b125
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2021-10-12 14:56:48 +0700

    Update VisitLangsung

--- lib/ActivityWidgets/HalamanVisit.dart
+++ lib/ActivityWidgets/HalamanVisit.dart
@@ -12,87 +12,165 @@ class _HalamanVisitState extends State<HalamanVisit> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            leading: Padding(
-              padding: EdgeInsets.only(left: 30.0,right: 0.0),
-              child: Image.asset('assets/icons/LogoVisitSekarang.png'),
-            ),
-            title: new Text('Visit Sekarang'),
-            backgroundColor: Color.fromRGBO(95, 37, 224, 100),
-          ),
+    return Scaffold(
 
-          body: Form(
-            child: Container(
-              padding: EdgeInsets.all(20.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Masukan Nama Lengkap Pasien',
-                          labelText: 'Nama Pasien',
-                          icon: Icon(Icons.people)),
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.only(left: 30.0,right: 0.0),
+          child: Image.asset('assets/icons/LogoVisitSekarang.png'),
+        ),
+        title: new Text('Visit Sekarang'),
+        backgroundColor: Color.fromRGBO(95, 37, 224, 100),
+      ),
+
+      body: Form(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 28.0, horizontal: 30.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  //Nama Pasien
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100),
+                        ),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      hintText: 'Masukan Nama Lengkap Pasien',
+                      labelText: 'Nama Pasien',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Masukan Usia Pasien',
-                          labelText: 'Usia Pasien',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Usia Pasien
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100),
+                        ),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      hintText: 'Masukan Usia Pasien',
+                      labelText: 'Usia Pasien',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Masukan Jenis Kelamin Pasien',
-                          labelText: 'Jenis Kelamin',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Jenis Kelamin
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100),
+                        ),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      hintText: 'Masukan Jenis Kelamin',
+                      labelText: 'Jenis Kelamin',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Masukan Keluhan Pasien',
-                          labelText: 'Keluhan Utama',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Keluhan Utama
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100),
+                        ),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      hintText: 'Masukan Keluhan Pasien',
+                      labelText: 'Keluhan Utama',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Masukan tanggal awal sakit',
-                          labelText: 'Keluhan Sejak',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Keluhan Sejak
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100),
+                        ),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      hintText: 'dd/mm/yyyy',
+                      labelText: 'Keluhan Sejak',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Masukan Jenis Kelamin Pasien',
-                          labelText: 'Diagnosa Dokter (Jika Ada)',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Diagnosa Dokter (Jika Ada)
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100),
+                        ),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      hintText: 'Jika ada',
+                      labelText: 'Diagnosa Dokter',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Kondisi Pasien Sekarang',
-                          labelText: 'Kondisi Pasien Sekarang',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Kondisi Pasien Sekarang
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100)
+                        ),
+                        borderRadius: BorderRadius.circular(5.0)
+                      ),
+                      hintText: 'Tirah Baring / Normal / Hanya Bisa Duduk',
+                      labelText: 'Nama Pasien',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Permintaan Khusus',
-                          labelText: 'Permintaan Khusus',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Permintaan Khusus
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      suffixIcon: Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                        size: 45.0,
+                        color: Color.fromRGBO(68, 105, 132, 100),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromRGBO(217, 223, 229, 100)
+                        ),
+                        borderRadius: BorderRadius.circular(5.0)
+                      ),
+                      hintText: 'Drop Down Bar',
+                      labelText: 'Permintaan Khusus',
                     ),
-                    TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: 'Dropdown',
-                          labelText: 'Pilih Tanggal Terapi',
-                          icon: Icon(Icons.people)),
+                  ),
+                  SizedBox(height: 10.0),
+                  //Pilih Tanggal Terapi
+                  TextFormField(
+                    decoration: new InputDecoration(
+                      suffixIcon: Image.asset('assets/icons/IconCalendar.png'),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(217, 223, 229, 100),
+                        ),
+                        borderRadius: BorderRadius.circular(5.0)
+                      ),
+                      hintText: 'Date Pop Up',
+                      labelText: 'Pilih Tanggal Terapi',
                     ),
-                    TextButton(
-                      onPressed: () {},
-                      child : Text('Lanjutkan Pemesanan'),
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 15.0),
+                  //Tombol "Lanjutkan Pemesanan"
+                  TextButton(
+                    onPressed: () {},
+                    child : Text('Lanjutkan Pemesanan'),
+                  ),
+                ],
               ),
             ),
-          ),
         ),
-      );
+      ),
+    );
   }
 }
