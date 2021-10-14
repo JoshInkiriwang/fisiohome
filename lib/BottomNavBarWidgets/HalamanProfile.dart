d936e462d933c6fb921c53d99bb379312c15685a
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2021-10-14 16:09:59 +0700

    Profile Page Update

--- lib/BottomNavBarWidgets/HalamanProfile.dart
+++ lib/BottomNavBarWidgets/HalamanProfile.dart
@@ -1,4 +1,7 @@
+import 'package:FisioHome/ColorPalette.dart';
 import 'package:flutter/material.dart';
+
+import 'HomePage/Components/HomeButtonGroup.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -17,8 +20,48 @@ class HalamanProfile extends StatelessWidget {
         ),
         body: new Container(
             padding: EdgeInsets.all(20.0),
-            child: new Center(
-              child: new Text('Test'),
-            )));
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset('assets/images/logoFisiohome.png'),
+                    ],
+                  ),
+                ),
+                const Divider(
+                  height: 20,
+               //   thickness: 5,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+                Container(
+                  height: 100.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      HomeButtonGroup(),
+                    ],
+                  ),
+                ),
+
+                const Divider(
+                  height: 20,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+
+                Expanded(
+                  child: Container(
+                    color: Colors.green,
+                    child: const Center(
+                      child: Text('Below'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+        ),
+    );
   }
 }
