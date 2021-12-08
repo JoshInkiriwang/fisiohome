6cbc9989c33a2285a63bb33481c2d994f836169b
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2021-12-08 13:59:27 +0700

    Updates on Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+import 'package:FisioHome/ActivityWidgets/HalamanPaket.dart';
+import 'package:FisioHome/ActivityWidgets/HalamanRiwayat.dart';
+import 'package:FisioHome/ActivityWidgets/Lifestyle Path/personalFisioterapisPage.dart';
+import 'package:FisioHome/ActivityWidgets/Lifestyle%20Path/EventPage.dart';
+import 'package:FisioHome/BottomNavBarWidgets/ActivityPage/HalamanActivity.dart';
+import 'package:FisioHome/BottomNavBarWidgets/ArticlePage/Detail%20Article.dart';
+import 'package:FisioHome/BottomNavBarWidgets/ArticlePage/HalamanArticle.dart';
+import 'package:FisioHome/BottomNavBarWidgets/ChatPage/HalamanChat.dart';
+import 'package:FisioHome/BottomNavBarWidgets/HomePage/HalamanHome.dart';
+import 'package:FisioHome/BottomNavBarWidgets/ProfilePage/HalamanProfile.dart';
 import 'package:FisioHome/IntroLogoOnly.dart';
+import 'package:FisioHome/LoginSection/LoginPage.dart';
+import 'package:FisioHome/OnBoarding/OnBoarding.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +22,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fisiohome',
-      home: LogoOnlyPage(),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LogoOnlyPage(),
+        '/onboarding': (context) => OnBoarding(),
+        '/login': (context) => LoginPage(),
+        // Register
+        '/home': (context) => HalamanHome(),
+        '/activitypage': (context) => HalamanActivity(),
+        '/articlepage': (context) => HalamanArticle(),
+        '/detailarticle': (context) => DetailArticle1(),
+        '/chatpage': (context) => HalamanChat(),
+        '/profilepage': (context) => HalamanProfile(),
+        '/eventpage': (context) => EventPage(),
+        '/personalfisioterapi': (context) => PersonalFisioterapisPage(),
+        '/paketPage': (context) => HalamanPaket(),
+        '/riwayatPage':  (context) => HalamanRiwayat(),
+      }
     );
   }
 }
